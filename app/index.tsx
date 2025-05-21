c9e6fb35e986b906e2abb1f9b1941dfc39c471c0
Rafael <raf.gomes.reis@gmail.com>
2025-05-21 16:47:47 -0300

    add-interface-loja

---
+++ app/index.tsx
@@ -0,0 +1,49 @@
+import { SafeAreaView, View, StyleSheet, Image, Text } from "react-native";
+import { Button } from "../components/buton";
+
+export default function Screen() {
+
+    const HandleStart = () => {
+        
+    }
+    
+    return (
+        <SafeAreaView style={styles.container}>
+            <Image
+                source={require('../assets/images.png')}
+                style={styles.logo}
+                resizeMode="cover"
+            />
+            <Text style={styles.title}>Interface de Loja do Maioral</Text>
+            <Text style={styles.subtitle}>Aqui tem de tudo e mais um pouco</Text>
+            <Button
+                title="Entrar"
+                onPress={HandleStart}
+            />
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    logo: {
+        width: 200,
+        height: 200,
+        marginBottom: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 5,
+    }, 
+    subtitle: {
+        fontSize: 16,
+        color: '#666',
+    },
+    
+});
---
+++ components/buton.tsx
@@ -0,0 +1,31 @@
+import { Pressable, View, Text, StyleSheet } from "react-native";
+
+type Props = {
+    title: string;
+    onPress: () => void;
+}
+
+
+export const Button = ({ title, onPress }: Props) =>{
+    return (
+        <Pressable onPress={onPress} style={styles.button}>
+            <Text style={styles.buttonText}>{title}</Text>
+        </Pressable>
+    )
+}
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: '#000',
+        padding: 10,
+        borderRadius: 5,
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+    }
+})  
